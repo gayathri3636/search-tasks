5676620c29b36a222fba6e8d6d8b7a2037a71684
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-07-20 06:27:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NamesContainer from "./NamesContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    names: [
+      "John",
+      "Abigail",
+      "Gayathri",
+      "Hima",
+      "Deepthi",
+      "jaanu",
+      "Andrew",
+      "Abhi",
+      "Gagan",
+    ],
+    searchTerm: "",
+  };
+
+  editSearchTerm = (e) => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  dynamicSearchTerm = (e) => {
+    return this.state.names.filter((name) =>
+      name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    );
+  };
+
+  render() {
+    return (
+      <div style={{ textAlign: "center", paddingTop: "30vh" }}>
+        <input
+          type="text"
+          value={this.state.searchTerm}
+          placeholder="search for a name"
+          onChange={this.editSearchTerm}
+        />
+        <br />
+        <h3>These are important names:</h3>
+        <NamesContainer names={this.dynamicSearchTerm()} />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Name.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Name extends React.Component {
+  render() {
+    return <div>{this.props.name}</div>;
+  }
+}
+export default Name;
